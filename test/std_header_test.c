26ea5a97c48beba20a9042b8fb030f88e6f20bf5
tomocy <tomocy.dev@gmail.com>
2021-06-19 14:57:33 +0900

    Add some std headers such as float.h

---
+++ test/std_header_test.c
@@ -0,0 +1,12 @@
+#include <float.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdnoreturn.h>
+
+#include "adapter.h"
+
+int main() {
+  ok();
+}
